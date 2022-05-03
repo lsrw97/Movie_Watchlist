edb67fe89b9941312e4be018903ca6d23bb87988
lsrw97 <eugen.el@yahoo.com>
2022-05-03 10:36:02 +0200

    add remove function

--- script.js
+++ script.js
@@ -78,8 +78,8 @@ function getMoviesListHtml(data){
         for (var i = 0; i < watchList.length; i++) {
             let id = watchList[i].id
             console.log(id)
-            watchList[i].addEventListener("click", function () {
-                console.log(watchList[i])
+            watchList[i].addEventListener("click", function (e) {
+                console.log(e.path[0].id)
                 myWatchlist.push(id)
                 localStorage.setItem('watchlist', JSON.stringify(myWatchlist))
             });
@@ -115,7 +115,7 @@ function getWatchlistHtml() {
                             <img src="${movieInfos.Poster}" id="movie-poster">
                             <div class="movie-info">
                                 <div class="info-title"><h2 id="movie-title">${movieInfos.Title}</h2><p id="movie-rating"><img src="images/star.png" class="star">${movieInfos.imdbRating}</p></div>
-                                <div class="info-specs"><span id="movie-duration">${movieInfos.Runtime}</span><span id="movie-genre">${movieInfos.Genre}</span><a id="${movie.Title}" class="add-watchlist"><img src="images/add.png" class="add-icon">Remove from Watchlist</a></div>
+                                <div class="info-specs"><span id="movie-duration">${movieInfos.Runtime}</span><span id="movie-genre">${movieInfos.Genre}</span><a id="${movieInfos.Title}" class="remove-watchlist"><img src="images/delete.png" class="add-icon">Remove from Watchlist</a></div>
                                 <div class="info-description"><span id="movie-description">${movieInfos.Plot}</span></div>
                             </div>
                         </div>
@@ -130,15 +130,35 @@ function getWatchlistHtml() {
                             <img src="${movieInfos.Poster}" id="movie-poster">
                             <div class="movie-info">
                                 <div class="info-title"><h2 id="movie-title">${movieInfos.Title}</h2><p id="movie-rating"><img src="images/star.png" class="star">${movieInfos.imdbRating}</p></div>
-                                <div class="info-specs"><span id="movie-duration">${movieInfos.Runtime}</span><span id="movie-genre">${movieInfos.Genre}</span><a id="${movie.Title}" class="add-watchlist"><img src="images/add.png" class="add-icon">Remove from Watchlist</a></div>
+                                <div class="info-specs"><span id="movie-duration">${movieInfos.Runtime}</span><span id="movie-genre">${movieInfos.Genre}</span><a id="${movieInfos.Title}" class="remove-watchlist"><img src="images/delete.png" class="add-icon">Remove from Watchlist</a></div>
                                 <div class="info-description"><span id="movie-description">${movieInfos.Plot}</span></div>
                             </div>
                         </div>
                     </div>`
                 }
             })
+            
+
     })
-        
+    setTimeout(() => {
+        const watchList = document.getElementsByClassName('remove-watchlist')
+        for (var i = 0; i < watchList.length; i++) {
+            watchList[i].addEventListener("click", function (e) {
+                const arr = JSON.parse(localStorage.getItem("watchlist"))
+                const index = arr.indexOf(e.path[0].id, 0)
+                console.log(e.path[0].id)
+                console.log(myWatchlist)
+                console.log(arr)
+                console.log(index)
+                arr.splice(index, 1)
+                console.log(arr)
+                localStorage.clear()
+                myWatchlist = arr
+                localStorage.setItem('watchlist', JSON.stringify(arr))
+            });
+        }
+    }, 1000)
+    
     searchFilmLink = document.getElementById('searchFilmLink')
     searchFilmLink.addEventListener('click', getIndexHtml)
 }
